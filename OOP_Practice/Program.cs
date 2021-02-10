6359a42966705a96768ed35c6b17eb682b490018
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-10 12:41:37 +0200

    Добавьте файлы проекта.

---
+++ OOP_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
